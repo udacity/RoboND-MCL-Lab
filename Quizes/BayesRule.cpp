a2b7f289382cd5d784d59ea400ee2e0b617fd76b
Karim Chamaa <karim@udacity.com>
2017-11-13 14:20:11 -0800

    Update BayesRule.cpp

--- Quizes/BayesRule.cpp
+++ Quizes/BayesRule.cpp
@@ -2,15 +2,15 @@
 using namespace std;
 
 int main() {
-	//Given P(C), P(POS|C) and P(POS|¬C)
-	double a = 0.0001; //P(C) = 0.001
-	double b = 0.8   ; //P(POS|C) = 0.8
-	double c = 0.1   ; //P(POS|¬C) = 0.1
+	//Given P(POS), P(DOOR|POS) and P(DOOR|¬POS)
+	double a = 0.0001; //P(POS) = 0.001
+	double b = 0.8   ; //P(DOOR|POS) = 0.8
+	double c = 0.1   ; //P(DOOR|¬POS) = 0.1
 	
-	//Compute P(¬C) and P(C|POS)
-	double d = 1-a; //P(¬C)
-	double e =  (b*a)/((a*b)+(d*c))  ; //P(C|POS)
+	//Compute P(¬POS) and P(POS|DOOR)
+	double d = 1-a; //P(¬POS)
+	double e =  (b*a)/((a*b)+(d*c))  ; //P(POS|DOOR)
 	
-	cout << "P(C|POS)= " <<    e    << endl;
+	cout << "P(POS|DOOR)= " <<    e    << endl;
 	return 0;
 }
